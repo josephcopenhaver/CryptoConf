3dfa8b30c8b515143aa157e2f5122c29c94b87e0
Joseph Copenhaver <joseph.copenhaver@gmail.com>
2015-05-14 02:06:37 -0500

    app_file path resolution was missing application parent dir routing also removed extra function call to fill datastructures

--- cryptoconf/app_files.py
+++ cryptoconf/app_files.py
@@ -8,44 +8,49 @@ from cryptoconf.settings import settings
 
 
 _g_file_store = None
-_file_store_lock = Lock()
+_g_pdir = None
+_threading_lock = Lock()
 
 _threading = local()
 _threading.file_store = None
-
-
-def _fill_file_store():
-	global _g_file_store
-
-	fpath = sys.argv[0]
-	pdir = path.dirname(fpath)
-	pdir = path.abspath(pdir)
-	pdir = path.normpath(pdir)
-
-	for setting in settings(pdir):
-		fpath = setting.raw_fpath
-		if not path.isabs(fpath):
-			path.join(pdir, fpath)
-		fpath = path.abspath(fpath)
-		fpath = path.normpath(fpath)
-		assert _g_file_store.get(fpath, None) is None
-		_g_file_store[fpath] = setting.pkey
+_threading.pdir = None
 
 
 def _init_threading():
 	global _g_file_store
+	global _g_pdir
 
-	with _file_store_lock:
-		file_store = _threading.file_store
-		if file_store is not None:
-			return file_store
-		if _g_file_store is not None:
-			_threading.file_store = _g_file_store
-			return _g_file_store
-		_g_file_store = {}
+	if _g_file_store is not None and _g_pdir is not None:
 		_threading.file_store = _g_file_store
-		_fill_file_store()
-		return _g_file_store
+		_threading.pdir = _g_pdir
+		return _g_file_store, _g_pdir
+
+	with _threading_lock:
+		if _g_file_store is not None and _g_pdir is not None:
+			_threading.file_store = _g_file_store
+			_threading.pdir = _g_pdir
+			return _g_file_store, _g_pdir
+		file_store = {}
+		pdir = sys.argv[0]
+		pdir = path.dirname(pdir)
+		pdir = path.abspath(pdir)
+		pdir = path.normpath(pdir)
+
+		for setting in settings(pdir):
+			fpath = setting.raw_fpath
+			if not path.isabs(fpath):
+				path.join(pdir, fpath)
+			fpath = path.abspath(fpath)
+			fpath = path.normpath(fpath)
+			assert file_store.get(fpath, None) is None
+			file_store[fpath] = setting.pkey
+			del fpath
+
+		_g_file_store = file_store
+		_g_pdir = pdir
+		_threading.file_store = file_store
+		_threading.pdir = pdir
+		return file_store, pdir
 
 
 def get_file_contents(fpath):
@@ -53,19 +58,22 @@ def get_file_contents(fpath):
 	Read from a cryptographically secure config file
 	returns bytes in the raw file
 	"""
-	abs_fpath = path.abspath(fpath)
-	abs_fpath = path.normpath(abs_fpath)
 	file_store = _threading.file_store
-	if file_store is None:
-		file_store = _init_threading()
+	pdir = _threading.pdir
+	if file_store is None or pdir is None:
+		file_store, pdir = _init_threading()
+	if not path.isabs(fpath):
+		fpath = path.join(pdir, fpath)
+	fpath = path.abspath(fpath)
+	fpath = path.normpath(fpath)
 
-	pkey = file_store.get(abs_fpath, None)
+	pkey = file_store.get(fpath, None)
 	
 	if pkey is None:
 		return None
 
 	bytes_buffer = BytesIO()
-	decrypt_single(pkey, abs_fpath, bytes_buffer)
+	decrypt_single(pkey, fpath, bytes_buffer)
 	bytes_buffer.seek(0)
 
 	return bytes_buffer.read()
